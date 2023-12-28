513754573e61988dc7b1b35afb7e67336b227828
ketingchen <89886222+ketingchen@users.noreply.github.com>
2023-12-28 16:04:09 -0600

    Update RunBPRF.R

--- RF_cluster_metabolome_association/RunBPRF.R
+++ RF_cluster_metabolome_association/RunBPRF.R
@@ -9,7 +9,7 @@
 #     -nt: nThreads, number of cpus requested
 #     -o: path to write the output (optional)
 #     -r: path to write the model evaluation report (optional)
-#   
+#     -l (optional): lasso penalty, ranging from 0 to 1
 require(randomForest)
 require(doParallel)
 
@@ -20,7 +20,7 @@ path=c("/work/LAS/myn-lab/KChen/BlockPC.RF/updateX.R",
 for(pa in path) source(pa)
 
 args = commandArgs(trailingOnly=T)
-lab=c("-x","-y","-b","-m","-c","-o","-r","-nt")
+lab=c("-x","-y","-b","-m","-c","-o","-r","-nt", "-l")
 idx=sapply(lab, function(ll)ifelse(sum(grepl(ll,args))==1,which(grepl(ll,args)),NA))
 args1=rep("",length(lab));names(args1)=lab
 for(i in 1:length(idx)){
@@ -42,7 +42,12 @@ cls=as.numeric(synt.cls[,"Gene_Cluster"])
 names(cls)=rownames(synt.cls)
 if(sum(cls==0)==0) cls[cls==max(cls)]=0
 
-bprf=block.pc.rf(X=x, Y=y, lv=1, cluster=cls,quality.check=F, package="randomForest")
+if(!is.na(args["-l"])){
+        l <- as.numeric(args["-l"])
+} else l <- 0
+
+	   
+bprf=block.pc.rf(X=x, Y=y, lv=1, lamda=l, cluster=cls,quality.check=F, package="randomForest")
 
 nt=as.numeric(args["-nt"])
 
